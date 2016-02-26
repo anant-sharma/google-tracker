fd3fd5e1a90f4485db8aa392c8e39b4ae3185735
anant-sharma <anantatnet@gmail.com>
2016-02-26 11:58:17 +0530

    v0.0.1

---
+++ server.js
@@ -0,0 +1,21 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
+});
+
+app.get('/app', function(req, res){
+  res.sendFile(__dirname + '/app.html');
+});
+
+io.on('connection', function(socket){
+  socket.on('new loc input', function(loc){
+    io.emit('new loc', loc);
+  });
+});
+
+http.listen(process.env.PORT, function(){
+  console.log('Server Started');
+});
